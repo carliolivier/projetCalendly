5e4ce868a43455ac17ba6cafbe070330e1205e81
carliolivier <olivier.carlier.foreach@gmail.com>
2023-02-22 11:14:05 +0100

    changement

--- cypress/e2e/connexion-a-un-compte.cy.js
+++ cypress/e2e/connexion-a-un-compte.cy.js
@@ -3,23 +3,19 @@
 
 describe("connexion reussi a un compte", () => {
 	it("connexion passante", () => {
-        cy.visit("https://calendly.com/");
-        cy.wait(3000);
-        cy.get('#onetrust-accept-btn-handler').click();
-        cy.wait(3000);
-        cy.get('#right-side-components > :nth-child(1) > .sc-1e4rkx0-0').click();
-        cy.wait(3000);
-        // cy.get('.sc-r1giur-0').type('zitoune59200@yahoo.com');
-        cy.get('.sc-r1giur-0').type('iammmahfoud@gmail.com', {force:true});
-        cy.get('[data-testid="primary-button"]').click({force:true});
-        cy.get('form').within(() => {
-                
-                cy.get('input[name="password"]').type('Test2023');
-                cy.get('button').find('div').click({force:true});
-                cy.wait(2000);
-        })
-
-        
+                cy.visit("https://calendly.com/");
+                cy.wait(3000);
+                cy.get('#onetrust-accept-btn-handler').click();
+                cy.wait(3000);
+                cy.get('#right-side-components > :nth-child(1) > .sc-1e4rkx0-0').click();
+                cy.wait(3000);
+                cy.get('.sc-r1giur-0').type('iammmahfoud@gmail.com', {force:true});
+                cy.get('[data-testid="primary-button"]').click({force:true});
+                cy.get('form').within(() => {
+                        cy.get('input[name="password"]').type('Test2023');
+                        cy.get('button').find('div').click({force:true});
+                        cy.wait(2000);
+                        })  
 	});
 
         it("Connexion non passante avec un mot de passe erroné", () => {
@@ -33,14 +29,11 @@ describe("connexion reussi a un compte", () => {
                 cy.get('.sc-r1giur-0').type('iammmahfoud@gmail.com', {force:true});
                 cy.get('[data-testid="primary-button"]').click({force:true});
                 cy.get('form').within(() => {
-                        
                         cy.get('input[name="password"]').type('test2023');
                         cy.get('button').find('div').click({force:true});
                         cy.wait(2000);
                         cy.contains('Invalid password.').should('be.visible')
-                })
-                
-                
+                })        
         });
         it("Connexion non passante avec un email erroné", () => {
                 cy.visit("https://calendly.com/");
@@ -49,13 +42,12 @@ describe("connexion reussi a un compte", () => {
                 cy.wait(3000);
                 cy.get('#right-side-components > :nth-child(1) > .sc-1e4rkx0-0').click();
                 cy.wait(3000);
-                // cy.get('.sc-r1giur-0').type('zitoune59200@yahoo.com');
                 cy.get('.sc-r1giur-0').type('iammmahfoudgmail.com', {force:true});
                 cy.get('[data-testid="primary-button"]').click({force:true});
-                cy.contains('Please enter a valid email address.').should('be.visible')
-               
-                
-                        
-                        });
+                cy.contains('Please enter a valid email address.').should('be.visible')         
+        });
+
+
+        
 });
 
--- cypress/e2e/creationEvenement.cy.js
+++ cypress/e2e/creationEvenement.cy.js
@@ -1,21 +1,24 @@
+
 describe('', () => {
-  it("Suppression d'un évenement", () => {
+  it("Création d'un évenement", () => {
     cy.visit(
       'https://calendly.com/users/confirmation?confirmation_token=VsSGnfaEmapBZpwKsSNm'
     );
     cy.get('[name="password"]').click().type('jonathanTestCalendly123');
     cy.contains('Continuer').click();
     cy.get('#onetrust-reject-all-handler').click();
-    cy.get('input[type="checkbox"]').each(($elem, index) => {
-      if (index === 1) {
-        cy.wrap($elem).click({ force: true });
-      }
-    });
     cy.get(
-      '[class="QDMFZL84dmkYn_0UnY8k RCy3C8tdsCfUkZ4tL6_j Y9hbVvdzlYyJiEWjJ9oy fmlLT3BGyLhEUKMImecT _drff0xP0ecktVJak07B _9FU_yg64fo4CH5kkNow cBJ4O_EakspfEJbJujzx"]'
+      '.js-new-event-type-region > :nth-child(1) > .M5lLwMMp9XR_Ejbv83RK > .clyOGp1YInfwtQ8ffY_1 > .M_4ZJy_elGKAc65Rl6DH > ._6n0CRU_Q9ef6f3sgBGZ'
     ).click();
     cy.get(
-      '.Q7UGjqL_GKkhwuE5SBMq > .tM53syywJyhDRyPUDkhB > ._6n0CRU_Q9ef6f3sgBGZ'
+      '[data-component="group"] > .qAwyEnpbreTCu2tzPMOi > ._QVEO42ALEjm3yr8WTJV'
     ).click();
+    cy.get('[name="name"]').click().type('Cypress');
+    cy.get('.PAft3yNcTUafvfdpD_me').click();
+    cy.contains('Google Meet').click();
+    cy.get('[name="inviteesLimit"]').type('4');
+    cy.contains('Suivant').click();
+    cy.contains('Suivant').click();
+    cy.contains('Retour').click();
   });
 });
